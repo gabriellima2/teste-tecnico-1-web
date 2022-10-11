8ab7d5dc792f1da9f7d0c0a7c55c83a5dac67b2c
Gabriel Lima <gabriel.rslima10@gmail.com>
2022-10-11 20:22:28 -0300

    feat: adiciona estilos para o footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,1 +1,24 @@
-export const Footer = () => <footer></footer>;
+import styled, { css } from "styled-components";
+
+const Container = styled.footer`
+	${({ theme }) => css`
+		width: 100vw;
+
+		padding: ${theme.spaces[4]};
+		margin-top: ${theme.spaces[10]};
+
+		text-align: center;
+
+		background-color: ${theme.colors.util};
+	`}
+`;
+
+const Author = styled.small`
+	font-weight: 500;
+`;
+
+export const Footer = () => (
+	<Container>
+		<Author>Gabriel - {new Date().getFullYear()}</Author>
+	</Container>
+);
